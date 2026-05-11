d909593390c1d9f68676e1be35019b719238d239
Joseph Estilo <estilojoseph@gmail.com>
2026-05-11 20:08:59 +0800

    Main menu update - created Settings_Panel (functionality not yet added) - created Credits_Panel - Back button prefab

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject creditsPanel;
+    
+
+    public void OnPlayClicked()
+    {
+        Debug.Log("Play clicked");
+
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnSettingsClicked()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void OnQuitClicked()
+    {
+        Debug.Log("Quit clicked");
+        Application.Quit();
+    }
+    public void onCreditsClicked()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+    public void OnBackClicked(GameObject currentPanel)
+    {
+        Transform parent = currentPanel.transform.parent;
+
+        currentPanel.SetActive(false);
+
+        if (parent.name == "Main_Canvas")
+        {
+            mainMenuPanel.SetActive(true);
+            return;
+        }
+
+        parent.gameObject.SetActive(true);
+    }
+}
