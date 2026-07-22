100f34ab0a417fa0ffbe2763c28d563c81a06f65
Jewel Galido <jewel.b.galido12@gmail.com>
2026-07-22 18:24:56 +0800

    main menu animation added

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
-
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
@@ -10,26 +9,29 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject mainMenuPanel;
     [SerializeField] private GameObject settingsPanel;
     [SerializeField] private GameObject creditsPanel;
-   
     [SerializeField] private GameObject howToPlayPanel;
+
+    [Header("Panel Visibility")]
+    [SerializeField] private CanvasGroup mainMenuCanvasGroup;
 
     [Header("Gamepad First Selected")]
     [SerializeField] private GameObject mainMenuFirstSelected;
     [SerializeField] private GameObject settingsFirstSelected;
     [SerializeField] private GameObject creditsFirstSelected;
-
-    
+    [SerializeField] private GameObject howToPlayFirstSelected;
 
     [Header("Settings")]
     [SerializeField] private Toggle fullscreenToggle;
-[SerializeField] private GameObject howToPlayFirstSelected;
 
     [Header("Blur")]
     [SerializeField] private Material screenBlurMaterial; // same M_ScreenBlur asset
-
     private static readonly int BlurSizeID = Shader.PropertyToID("_BlurSize");
 
-private void Start()
+    [Header("Intro Animation")]
+    [SerializeField] private Animator mainMenuAnimator; // MainMenu_Panel's Animator
+    private static readonly int PlayIntroID = Animator.StringToHash("PlayIntro");
+
+    private void Start()
     {
         if (screenBlurMaterial != null)
             screenBlurMaterial.SetFloat(BlurSizeID, 0f);
@@ -40,6 +42,9 @@ private void Start()
             fullscreenToggle.onValueChanged.AddListener(SetFullscreen);
         }
 
+        if (mainMenuAnimator != null)
+            mainMenuAnimator.SetTrigger(PlayIntroID);
+
         EventSystem.current.SetSelectedGameObject(mainMenuFirstSelected);
     }
 
@@ -47,15 +52,27 @@ private void Start()
     {
         Gamepad pad = Gamepad.current;
         if (pad == null) return;
-
         // Circle = back, only if a sub-panel is open
         if (pad.buttonEast.wasPressedThisFrame)
         {
             if (settingsPanel.activeSelf) OnBackClicked(settingsPanel);
             else if (creditsPanel.activeSelf) OnBackClicked(creditsPanel);
-           
             else if (howToPlayPanel.activeSelf) OnBackClicked(howToPlayPanel);
         }
+    }
+
+    private void ShowMainMenu()
+    {
+        mainMenuCanvasGroup.alpha = 1f;
+        mainMenuCanvasGroup.interactable = true;
+        mainMenuCanvasGroup.blocksRaycasts = true;
+    }
+
+    private void HideMainMenu()
+    {
+        mainMenuCanvasGroup.alpha = 0f;
+        mainMenuCanvasGroup.interactable = false;
+        mainMenuCanvasGroup.blocksRaycasts = false;
     }
 
     public void OnPlayClicked()
@@ -63,15 +80,16 @@ private void Start()
         Debug.Log("Play clicked at time: " + Time.realtimeSinceStartup);
         SceneManager.LoadScene("MainGameIntroCutScene");
     }
-public void OnSettingsClicked()
+
+    public void OnSettingsClicked()
     {
-        mainMenuPanel.SetActive(false);
+        HideMainMenu();
         settingsPanel.SetActive(true);
         if (fullscreenToggle != null) fullscreenToggle.SetIsOnWithoutNotify(Screen.fullScreen);
         EventSystem.current.SetSelectedGameObject(settingsFirstSelected);
     }
 
-public void SetFullscreen(bool value)
+    public void SetFullscreen(bool value)
     {
         Screen.fullScreen = value;
     }
@@ -81,34 +99,31 @@ public void SetFullscreen(bool value)
         Debug.Log("Quit clicked");
         Application.Quit();
     }
+
     public void onCreditsClicked()
     {
-        mainMenuPanel.SetActive(false);
+        HideMainMenu();
         creditsPanel.SetActive(true);
         EventSystem.current.SetSelectedGameObject(creditsFirstSelected);
     }
+
     public void OnBackClicked(GameObject currentPanel)
     {
         Transform parent = currentPanel.transform.parent;
         currentPanel.SetActive(false);
         if (parent.name == "Main_Canvas")
         {
-            mainMenuPanel.SetActive(true);
+            ShowMainMenu();
             EventSystem.current.SetSelectedGameObject(mainMenuFirstSelected);
             return;
         }
         parent.gameObject.SetActive(true);
         EventSystem.current.SetSelectedGameObject(mainMenuFirstSelected);
     }
-    /*public void OnContinueClicked()
-    {
-        mainMenuPanel.SetActive(false);
-        continuePanel.SetActive(true);
-        EventSystem.current.SetSelectedGameObject(continueFirstSelected);
-    } */
+
     public void OnHowToPlayClicked()
     {
-        mainMenuPanel.SetActive(false);
+        HideMainMenu();
         howToPlayPanel.SetActive(true);
         EventSystem.current.SetSelectedGameObject(howToPlayFirstSelected);
     }
